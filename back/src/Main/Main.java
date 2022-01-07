8efc61f3dd30df8203bd9ffd11ea143af3c73677
vndan <vndan@DESKTOP-963DV6T>
2022-01-07 23:33:20 +0900

    첫날+1

--- back/src/Main/Main.java
+++ back/src/Main/Main.java
@@ -12,6 +12,6 @@ public class Main {
 	        
 	    }
 	    
-	      System.out.println(y+31);
+	      System.out.println(y);
 	    }
 	}
--- back/src/back/java2.java
+++
@@ -1,9 +0,0 @@
-package back;
-
-public class java2 {
-
-	public static void main(String[] args) {
-		System.out.println("hello world!");
-	}
-
-}
