caf338d495f8a06df094033eff96cb24f2b02dcd
DESKTOP-URI21C7\JHG <vndanwl@naver.com>
2022-06-04 15:27:24 +0900

    aa

---
+++ back/src/book/023.java
@@ -0,0 +1,13 @@
+package book;
+
+// 백준 11724 방향없는 그래프가 주어 졌을대  연결 요소의 개수를 구하시오
+public class book {
+
+    public static void main(String[] args)  {
+        java.io.BufferedReader br =
+
+
+
+    }
+}
+
